a5e7e12a71bf87b7ffafa22d25ab8388cb0b0592
FyuroTEX <yurilev4@gmail.com>
2018-09-10 21:26:46 +0300

    rep bugs

--- js.js
+++ js.js
@@ -1,4 +1,20 @@
 $(function() {
+  var $backTop = $(".back-top");
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= 20) {
+      $backTop.fadeIn();
+    } else {
+      $backTop.fadeOut();
+    }
+  });
+  $backTop.on("click", function() {
+    $("html,body").animate(
+      {
+        scrollTop: 0
+      },
+      900
+    );
+  });
   $(".map").maphilight({
     fillColor: "008800"
   });
